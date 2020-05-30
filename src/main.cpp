982ed1867b871bac1ae7ba1f7a9565b3d118ddc1
LSK <lsk@xps15tv>
2020-05-30 16:13:27 +0200

    bf fix paths

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,61 @@
 using namespace std;
 using namespace tredzone;
 
+/*
+ * Event that will be sent from the WriterActor to the PrinterActor.
+ * The payload is a string that will be printed to the console.
+ */
+struct PrintEvent : Actor::Event
+{
+	PrintEvent(const string& message)
+        : message(message)
+    {
+	}
+    
+	const string message;
+};
+
+// when PrinterActor receives PrintEvents it displays them to the console
+class PrinterActor : public Actor
+{
+public:
+
+	// service tag (or key) used to uniquely identify service
+	struct ServiceTag : Service {};
+
+    // ctor
+	PrinterActor()
+    {
+		cout << "PrinterActor::PrinterActor()" << endl;
+		registerEventHandler<PrintEvent>(*this);
+    }
+    
+	// called when PrintEvent is received
+	void onEvent(const PrintEvent& event)
+    {
+		cout << "PrinterActor::onEvent(): " << event.message << ", from " << event.getSourceActorId() << endl;
+	}
+};
+
+// WriterActor sends a PrintEvent containing a string to PrinterActor
+class WriterActor : public Actor
+{
+public:
+    // ctor
+    WriterActor()
+    {
+		cout << "WriterActor::CTOR()" << endl;
+        
+        // retrieve PrinterActor's id from the ServiceIndex
+		const ActorId&   printerActorId = getEngine().getServiceIndex().getServiceActorId<PrinterActor::ServiceTag>();
+        
+		Event::Pipe pipe(*this, printerActorId);	// create uni-directional communication channel between WriterActor (this) and PrinterActor (printerActorId)
+		pipe.push<PrintEvent>("Hello, World!");		// send PrintEvent through pipe
+	}
+};
+
+//---- Main --------------------------------------------------------------------
+
 int main()
 {
     cout << "tutorial #4 : printer actor service" << endl;
