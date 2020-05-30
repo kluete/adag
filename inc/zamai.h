f7f9930818a23d9be6b875f5c656498a66fe757e
LSK <lsk@xps15tv>
2020-05-30 20:24:56 +0200

    InitDAGNodes not yet implemented

--- inc/zamai.h
+++ inc/zamai.h
@@ -7,13 +7,16 @@ namespace zamai
 {
 // import into namespace
 using tredzone::Actor;
-    
+
+//---- DAG interface (a la Scott Meyers) ---------------------------------------
+
 class IDag
 {
 public:
 
     virtual ~IDag() = default;
     
+    virtual void    InitDAGNodes(void) = 0;
     virtual void    SetIndexActorId(const size_t &i, const Actor::ActorId &actor_id) = 0;
     virtual void    next(void) = 0;
 
--- src/dag.cpp
+++ src/dag.cpp
@@ -17,9 +17,16 @@ class DAGImp : public IDag
 public:
     // ctor
     DAGImp(const size_t max_nodes, const size_t max_node_fanning)
+        : m_MaxNodes(max_nodes), m_MaxNodeFans(max_node_fanning)
     {
         // generate index -> index graph
     }
+    
+    void    InitDAGNodes(void) override
+    {
+        
+    }
+    
     
     void    SetIndexActorId(const size_t &i, const Actor::ActorId &actor_id) override
     {
@@ -34,7 +41,8 @@ public:
 
 private:
     
-    
+    const size_t    m_MaxNodes;
+    const size_t    m_MaxNodeFans;
 };
 
 //---- INSTANTIATION -----------------------------------------------------------
