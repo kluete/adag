80233f4d10865082284cd3c4a5ed44fd95804f9d
LSK <lsk@xps15tv>
2020-06-01 21:47:04 +0200

    op bias (to avoid zero trail)

--- src/main.cpp
+++ src/main.cpp
@@ -37,22 +37,18 @@ ComputeInit
 public:
 
     // ctor
-    ComputeInit(IDag *dag, const size_t index, const uint32_t &opmul)
-        : m_IDag(dag), m_Index(index), m_OpMul(opmul)
+    ComputeInit(IDag *dag, const size_t index, const uint32_t &opmul, const uint32_t &opbias)
+        : m_IDag(dag), m_Index(index), m_OpMul(opmul), m_OpBias(opbias)
     {
         assert(dag);
-    }
-    
-    void    RegisterActorId(void)
-    {
-        
     }
     
 private:
 
     IDag            *m_IDag;
-    const size_t    m_Index;
+    const size_t    m_Index;            // position in DAG tab
     const uint32_t  m_OpMul;    
+    const uint32_t  m_OpBias;
 };
 
 //---- Compute Node actor ------------------------------------------------------
@@ -63,7 +59,8 @@ public:
 
     // ctor
 	ComputeActor(const ComputeInit &init)
-        : m_Dag(init.m_IDag), m_Index(init.m_Index), m_OpMul(init.m_OpMul)
+        : m_Dag(init.m_IDag), m_Index(init.m_Index),
+        m_OpMul(init.m_OpMul), m_OpBias(init.m_OpBias)
     {
 		cout << "ComputeActor::ComputeActor()" << endl;
         
@@ -79,24 +76,14 @@ public:
 		cout << "ComputeActor::onEvent(): " << e.m_Val << ", from " << e.getSourceActorId() << endl;
         
         // apply computation
-        const uint64_t  rolling = (uint64_t) (e.m_Val * m_OpMul);
+        const uint64_t  rolling = (uint64_t) ((e.m_Val * m_OpMul) + m_OpBias);
         
-        const vector<size_t>    child_nodes = m_Dag->GetChildNodes(m_Index);
-        
-        // send to child nodes
-        for (size_t child_id: child_nodes)
-        {
-            const Actor::ActorId     child_actor_id = m_Dag->GetNodeActorId(child_id);
-            
-            Event::Pipe pipe_to_child(*this, child_actor_id);
-            
-            pipe_to_child.push<ComputeEvent>(rolling);
-        }
+        BroadcastToChildren(rolling);
 	}
     
     void onCallback()
     {
-		cout << "registering actor ID " << m_Index << endl;
+		// cout << "registering actor ID " << m_Index << endl;
         
         // can register now that has true core position
         const ActorId    aid = getActorId();
@@ -112,9 +99,30 @@ public:
     
 private:
 
+    // trickle-down to children
+    void    BroadcastToChildren(const uint64_t val)
+    {
+        const vector<size_t>    child_nodes = m_Dag->GetChildNodes(m_Index);
+        if (child_nodes.empty())
+        {
+            cout << " NO MORE CHILD NODES" << endl;
+            return;
+        }
+        
+        // send to child nodes
+        for (size_t child_id: child_nodes)
+        {
+            const Actor::ActorId     child_actor_id = m_Dag->GetNodeActorId(child_id);
+            
+            Event::Pipe pipe_to_child(*this, child_actor_id);
+            
+            pipe_to_child.push<ComputeEvent>(val);
+        }
+    }
+
     IDag            *m_Dag;
     const size_t    m_Index;
-    const uint32_t  m_OpMul;
+    const uint32_t  m_OpMul, m_OpBias;
 };
 
 //---- Main --------------------------------------------------------------------
@@ -136,8 +144,9 @@ int main()
         assert(cpu_core_id < ROOT_NODES);
         
         const uint32_t  op_mul = rnd_gen() * 0xffffu;
+        const uint32_t  op_bias = rnd_gen() * 0xffffu;
         
-        startSequence.addActor<ComputeActor>(cpu_core_id, ComputeInit(IDag.get(), i, op_mul));
+        startSequence.addActor<ComputeActor>(cpu_core_id, ComputeInit(IDag.get(), i, op_mul, op_bias));
     }
     
     Engine engine(startSequence);	                // start above actors
