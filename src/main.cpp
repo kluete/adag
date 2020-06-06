dba7c9f37d2bc11e8ab90ac5166982a47bec1692
LSK <lsk@xps15tv>
2020-06-07 00:14:23 +0200

    toghter bucket size, still infinite loop

--- src/main.cpp
+++ src/main.cpp
@@ -15,7 +15,7 @@
 
 constexpr uint32_t  TOTAL_NODES                 = 1'000;
 constexpr uint32_t  ROOT_NODES                  = 2;                    // same as # of DAG "entry points", should be slightly smaller than # CPU cores
-constexpr uint32_t  RANDOM_BUCKET_SIZE          = 10;
+constexpr uint32_t  RANDOM_BUCKET_SIZE          = 5;
 constexpr uint32_t  NODE_REGISTRATION_LOG_BATCH = 10000;                 // how often to log to cout
 
 
